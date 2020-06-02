0944cef92aa81e0de097e9a32463f3febf17f4ba
Yuan17608474816 <17608474816@163.com>
2020-06-02 22:45:21 +0800

    静态页面的编写 6/2

---
+++ 毕业项目源码和数据库/源码/AnTen/AnTen/Controllers/AT_LoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AnTen.Controllers
+{
+    public class AT_LoginController : Controller
+    {
+        // GET: AT_Login
+        public ActionResult Default()
+        {
+            return View();
+        }
+    }
+}
