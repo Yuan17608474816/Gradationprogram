8ab1168667e5ffa33453cd87177689c35e764d8b
Yuan17608474816 <17608474816@163.com>
2020-06-12 14:46:01 +0800

    静态界面的编写(6月12号)

---
+++ 毕业项目源码和数据库/源码/AnTen/AnTen/Controllers/AT_PMainController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AnTen.Controllers
+{
+    public class AT_PMainController : Controller
+    {
+        // GET: AT_PMain
+        public ActionResult DefMain()
+        {
+            return View();
+        }
+        public ActionResult PanMain()
+        {
+            return View();
+        }
+    }
+}
